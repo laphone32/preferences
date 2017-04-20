e8470595edd2997349573b1990965a94d23d7a4e
Laphone Lin <llin@timelinecapital.com>
2017-04-20 12:59:08 +0800

    Improvements

--- bin/term_util.sh
+++ bin/term_util.sh
@@ -2,7 +2,7 @@
 
 SETTERM=xtermcontrol
 
-export LAPHONE_DEFAULT_TERM="$SETTERM --bg=#555557575353 --title=[LOCAL]"
-export LAPHONE_PRODUCTION_TERM="$SETTERM --bg=#725817a117a1 --title=>>>PROD<<<"
-export LAPHONE_REMOTE_TERM="$SETTERM --bg=#34346565a4a4 --title=>>>REMOTE<<<"
+export LAPHONE_DEFAULT_TERM="$SETTERM --fg=white --bg=#555557575353 --title=[LOCAL]"
+export LAPHONE_PRODUCTION_TERM="$SETTERM --fg=white --bg=#725817a117a1 --title=>>>PROD<<<"
+export LAPHONE_REMOTE_TERM="$SETTERM --fg=white --bg=#34346565a4a4 --title=>>>REMOTE<<<"
 
