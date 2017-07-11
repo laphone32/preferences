fb9be5d8457a2aa62ea3497919c6287eb9ac153c
Laphone <llin@timelinecapital.com>
2017-07-11 10:39:50 +0800

    Darker color

--- bin/term_util.sh
+++ bin/term_util.sh
@@ -3,7 +3,8 @@
 SETTERM=xtermcontrol
 
 export LAPHONE_DEFAULT_TERM="$SETTERM --fg=white --bg=#555557575353 --title=[LOCAL]"
-export LAPHONE_PRODUCTION_TERM="$SETTERM --fg=white --bg=#725817a117a1 --title=>>>PROD<<<"
+#export LAPHONE_PRODUCTION_TERM="$SETTERM --fg=white --bg=#725817a117a1 --title=>>>PROD<<<"
+export LAPHONE_PRODUCTION_TERM="$SETTERM --fg=white --bg=#691B1B --title=>>>PROD<<<"
 export LAPHONE_UAT_TERM="$SETTERM --fg=white --bg=#245524 --title=>>>UAT<<<"
 export LAPHONE_REMOTE_TERM="$SETTERM --fg=white --bg=#34346565a4a4 --title=>>>REMOTE<<<"
 
