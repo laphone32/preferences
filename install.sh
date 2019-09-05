f97064b3734bc5127d7f7291df8f506a5a80d2ca
jlin <jlin@folio-sec.com>
2019-09-05 16:47:42 +0900

    Improvements
    * install script for better initialised
    * local gitignore for every local repos

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PREFERENCES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+BASH_PROFILE_NAME="$HONE/.bashrc"
+if [ ! -f $BASH_PROFILE_NAME ]; then
+    BASH_PROFILE_NAME="$HOME/.bash_profile"
+fi
+
+if [ -f $BASH_PROFILE_NAME ]; then
+    echo "#laphone preferences" >> $BASH_PROFILE_NAME
+    echo "PREFERENCES_DIR=$PREFERENCES_DIR" >> $BASH_PROFILE_NAME
+    echo "PREFERENCES_BIN=$PREFERENCES_DIR/bin" >> $BASH_PROFILE_NAME
+    echo "source $PREFERENCES_DIR/bashrc" >>  $BASH_PROFILE_NAME
+fi
+
+git config --global core.excludesfile $PREFERENCES_DIR/gitignore
+
