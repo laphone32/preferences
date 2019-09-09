9697e61cbc7384ea607ea74d1685d063a8f620fc
Laphone <laphone32@gmail.com>
2019-09-09 21:14:48 +0900

    Typo fixed

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 PREFERENCES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-BASH_PROFILE_NAME="$HONE/.bashrc"
+BASH_PROFILE_NAME="$HOME/.bashrc"
 if [ ! -f $BASH_PROFILE_NAME ]; then
     BASH_PROFILE_NAME="$HOME/.bash_profile"
 fi
@@ -12,6 +12,7 @@ if [ -f $BASH_PROFILE_NAME ]; then
     echo "PREFERENCES_DIR=$PREFERENCES_DIR" >> $BASH_PROFILE_NAME
     echo "PREFERENCES_BIN=$PREFERENCES_DIR/bin" >> $BASH_PROFILE_NAME
     echo "source $PREFERENCES_DIR/bashrc" >>  $BASH_PROFILE_NAME
+    echo "" >>  $BASH_PROFILE_NAME
 fi
 
 git config --global core.excludesfile $PREFERENCES_DIR/gitignore
