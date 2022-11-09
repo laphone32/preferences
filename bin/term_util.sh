15e9679409fc476fc6849fa08c5bcdadcebe560f
laphone <laphone32@gmail.com>
2022-11-09 23:01:24 +0900

    vim background update

--- bin/term_util.sh
+++ bin/term_util.sh
@@ -7,7 +7,7 @@ function setTerm {
 }
 
 export LAPHONE_DEFAULT_TERM="setTerm white #555557575353 [LOCAL]"
-export LAPHONE_VIM_TERM="setTerm white #393b39 [Vim]"
+export LAPHONE_VIM_TERM="setTerm white #213440 [Vim]"
 export LAPHONE_PRODUCTION_TERM="setTerm white #691B1B >PROD<"
 export LAPHONE_UAT_TERM="setTerm white #245524 >UAT<"
 export LAPHONE_REMOTE_TERM="setTerm white #34346565a4a4 >REMOTE<"
