d2a798d37b37b4821258e3c044fc3fd68fcd6080
laphone <laphone32@gmail.com>
2022-11-12 16:21:09 +0900

    Cleanup

--- term/color/iterm2.sh
+++ term/color/iterm2.sh
@@ -1,35 +1,28 @@
 #/bin/bash
 
 function setColor {
-    local profile title
+    local profile
 
     case $1 in
         "vim" )
             profile="Vim"
-            title="[Vim]"
             ;;
         "prod" )
             profile="Prod"
-            title="> PROD <"
             ;;
         "uat" )
             profile="UAT"
-            title="> UAT <"
             ;;
         "remote" )
             profile="Remote"
-            title="> REMOTE <"
             ;;
         "docker" )
             profile="Container"
-            title="> Container <"
             ;;
         *)
             profile="Default"
-            title="[LOCAL]"
             ;;
     esac
     echo -ne "\033]50;SetProfile=$profile\a"
-    echo -ne "\033]0;$title\007"
 }
 
--- term/color/xtermcontrol.sh
+++ term/color/xtermcontrol.sh
@@ -1,40 +1,34 @@
 #/bin/bash
 
 function setColor {
-    local file="" foreground="white" background title
+    local file="" foreground="white" background
     local colorPath="$PREFERENCES_TERM/color/xterm"
 
     case $1 in
         "vim")
             file="nord-xtermcontrol"
-            title="[Vim]"
             ;;
         "prod")
             background="#691B1B"
-            title="> PROD <"
             ;;
         "uat")
             background="#245524"
-            title="> UAT <"
             ;;
         "remote")
             background="#34346565a4a4"
-            title="> REMOTE <"
             ;;
         "docker")
             background="#0b0a2b"
-            title="> Container <"
             ;;
         *)
             background="#555557575353"
-            title="[LOCAL]"
             ;;
     esac
 
     if [[ $file == "" ]]; then
-        xtermcontrol --fg=$foreground --bg=$background --title=$title
+        xtermcontrol --fg=$foreground --bg=$background
     else
-        xtermcontrol --file=$colorPath/$file --title=$title
+        xtermcontrol --file=$colorPath/$file
     fi
 }
 
