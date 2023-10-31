3dae3891b59d4545d97752a653d084653ff1a18c
laphone <laphone32@gmail.com>
2023-10-31 19:09:34 +0900

    Minor update

--- systemd/max_battery_limit.sh
+++ systemd/max_battery_limit.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-DRIVER='/sys/class/power_supply/CMB0/charge_control_end_threshold'
+DRIVER1='/sys/class/power_supply/CMB0/charge_control_end_threshold'
+DRIVER2='/sys/devices/platform/lg-laptop/battery_care_limit'
+
+if [ -f $DRIVER1 ]; then
+    DRIVER=$DRIVER1
+else
+    DRIVER=$DRIVER2
+fi
+
 CONFIG='/etc/tmpfiles.d/max_battery_limit.conf'
 
 if [ -f $DRIVER ]; then
