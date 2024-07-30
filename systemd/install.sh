aa5142ab901c4f065cd0e696ea03e658fec35687
Laphone <laphone32@gmail.com>
2024-07-30 17:44:48 +0900

    Minor update

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
 PREFERENCES_SYSTEMD=$PREFERENCES_DIR/systemd
+running=$(ps --no-headers -o comm 1)
 
-#$PREFERENCES_SYSTEMD/disable_turbo_boost.sh
-$PREFERENCES_SYSTEMD/disable_silent_fan_mode.sh
-$PREFERENCES_SYSTEMD/max_battery_limit.sh
+if [ $? -eq 0 ] && [ $running == 'systemd' ]; then
+    echo "Installing systemd preferencs settings"
+
+    #$PREFERENCES_SYSTEMD/disable_turbo_boost.sh
+    $PREFERENCES_SYSTEMD/disable_silent_fan_mode.sh
+    $PREFERENCES_SYSTEMD/max_battery_limit.sh
+
+    # preferences settings
+    sudo mkdir -p /etc/systemd/logind.conf.d/
+    sudo ln -sf $PREFERENCES_SYSTEMD/logind.conf /etc/systemd/logind.conf.d/preferences.conf
+fi
+
 
 
