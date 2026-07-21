9bccccdd50bed738ced10c842cef78c99c4a830b
Laphone <laphone32@gmail.com>
2026-07-21 11:36:48 +0800

    Add linux distro detection

--- util/environment.sh
+++ util/environment.sh
@@ -19,6 +19,62 @@ function currentOs {
 }
 PREFERENCES_OS=$(currentOs)
 
+function currentDistro {
+    case $PREFERENCES_OS in
+        'Darwin')
+            echo 'macos'
+            ;;
+        'Windows')
+            echo 'windows'
+            ;;
+        *)
+            local os_file=""
+            if [ -f /etc/os-release ]; then
+                os_file="/etc/os-release"
+            elif [ -f /usr/lib/os-release ]; then
+                os_file="/usr/lib/os-release"
+            fi
+
+            if [ -n "$os_file" ]; then
+                local distro
+                distro=$(grep -E '^ID=' "$os_file" | cut -d= -f2 | tr -d '"' | tr '[:upper:]' '[:lower:]')
+                echo "${distro:-linux}"
+            else
+                echo "linux"
+            fi
+            ;;
+    esac
+}
+PREFERENCES_DISTRO=$(currentDistro)
+
+function currentDistroLike {
+    case $PREFERENCES_OS in
+        'Darwin')
+            echo 'macos'
+            ;;
+        'Windows')
+            echo 'windows'
+            ;;
+        *)
+            local os_file=""
+            if [ -f /etc/os-release ]; then
+                os_file="/etc/os-release"
+            elif [ -f /usr/lib/os-release ]; then
+                os_file="/usr/lib/os-release"
+            fi
+
+            if [ -n "$os_file" ]; then
+                local distro_like
+                distro_like=$(grep -E '^ID_LIKE=' "$os_file" | cut -d= -f2 | tr -d '"' | tr '[:upper:]' '[:lower:]')
+                echo "${distro_like:-$PREFERENCES_DISTRO}"
+            else
+                echo "$PREFERENCES_DISTRO"
+            fi
+            ;;
+    esac
+}
+PREFERENCES_DISTRO_LIKE=$(currentDistroLike)
+
 function currentLinuxDesktopEnvironment {
     local checkTags=('xfc' 'kde' 'unity' 'gnome' 'cinnamon' 'mate' 'deepin' 'budgie' 'lxqt')
     local de=''
