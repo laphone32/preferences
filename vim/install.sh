758a9b5c0b27942b6b176416eb5c380950d2d1bb
Laphone <laphone32@gmail.com>
2026-07-31 11:18:17 +0800

    (vim): installation improvement

--- vim/install.sh
+++ vim/install.sh
@@ -8,3 +8,10 @@ installPreferencesDir $HOME/.vim
 # Ideavim
 installPreferencesSymlink $PREFERENCES_DIR/vim/ideavimrc $HOME/.ideavimrc
 
+# Pre-install vim plugins
+if command -v vim &> /dev/null; then
+    echo "Installing Vim plugins..."
+    vim -u "$PREFERENCES_DIR/vim/vimrc" --not-a-term +PlugInstall +qall &> /dev/null || true
+fi
+
+
